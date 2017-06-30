40d6ec9d7609dbd98a50e42fbbe28d95e590e084
Bocong Zhao <bo@Bocongs-MacBook-Pro.local>
2017-06-30 11:34:47 -0500

    Initial setup githubbattle project

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+require('./index.css');
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+
+
